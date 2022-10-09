e3ca7e66ce2519db144ebddbe7df95ce9dd43a1b
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-10-09 13:35:57 +0200

    well

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zordon Site</title>
-</head>
-<body>
-<div class="topnav">
-    <img src="assets/logo.png" />
-    <a href="index.html">Home</a>
-    
-    <a href="http://forum.azordon.cf/">Forum</a>
-</div>
-
-<div class="center">
-
-</div>
-</body>
-</html>
+<?php
+if ('pl' == substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2)) {
+  header('location: pl/index.php');
+} else {
+  header('location: en/index.php');
+}
+?>
---
+++ pl/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zordon Site</title>
+    <meta name="description" content="AZordon Official Site">
+	<meta name="keywords" content="Dev, Developing, Developer, Deweloper">
+	<meta name="author" content="Zordon">
+	<meta http-equiv="X-Ua-Compatible" content="IE=edge">
+	
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="main.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+</head>
+<body>
+<div class="topnav">
+    <img src="assets/logo.png" />
+    <a href="index.html">Strona Główna</a>
+    <a href="http://forum.azordon.cf/">Forum</a>
+</div>
+
+<header>
+	
+		<nav class="navbar navbar-dark bg-jumpers navbar-expand-lg">
+		
+			<a class="navbar-brand" href="#"><img src="img/logo.png" width="30" height="30" class="d-inline-block mr-1 align-bottom" alt=""> Azordon.cf</a>
+		
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainmenu" aria-controls="mainmenu" aria-expanded="false" aria-label="Schowaj">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+		
+			<div class="collapse navbar-collapse" id="mainmenu">
+			
+				<ul class="navbar-nav mr-auto">
+				
+					<li class="nav-item active">
+						<a class="nav-link" href="#"> Start </a>
+					</li>
+					
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-expanded="false" id="submenu" aria-haspopup="true"> Programowanie </a>
+						
+						<div class="dropdown-menu" aria-labelledby="submenu">
+						
+							<a class="dropdown-item" href="./programowanie/csharp"> C# </a>
+							<a class="dropdown-item" href="./programowanie/cpp"> C++ </a>
+                            <a class="dropdown-item" href="./programowanie/lua"> Lua </a>
+							
+							<div class="dropdown-divider"></div>
+							
+							<a class="dropdown-item" href="./programowanie/php"> PHP </a>
+							<a class="dropdown-item" href="./programowanie/html5"> HTML5 </a>
+                            <a class="dropdown-item" href="./programowanie/css"> CSS </a>
+						
+						</div>
+						
+					</li>
+					
+					<li class="nav-item">
+						<a class="nav-link" href="#"> Historia </a>
+					</li>
+					
+					<li class="nav-item">
+						<a class="nav-link" href="#"> Zdjęcia </a>
+					</li>
+					
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#"> Wywiady </a>
+					</li>
+					
+					<li class="nav-item">
+						<a class="nav-link" href="#"> Kontakt </a>
+					</li>
+				
+				</ul>
+			
+				<form class="form-inline">
+				
+					<input class="form-control mr-1" type="search" placeholder="Wyszukaj" aria-label="Wyszukaj">
+					<button class="btn btn-light" type="submit">Znajdź</button>
+				
+				</form>
+			
+			</div>
+		
+		</nav>
+	
+	</header>
+</div>
+</body>
+</html>
