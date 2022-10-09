7cef02191185c6cf2bfddf8b6b40fa5667830baf
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-10-09 20:44:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 if ('pl' == substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2)) {
   header('location: pl/index.php');
 } else {
-  header('location: en/index.php');
+  echo "Sorry but English translate is still not done... you can view <a href='pl/index.php'>polish version of site</a>";
 }
 ?>
